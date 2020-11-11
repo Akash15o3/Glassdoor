0d3bfe2647aa592ca4c66c0a0f21d37d49317417
Krutika <krutikavk@gmail.com>
2020-11-10 17:17:01 -0800

    Krutika: gitignore changes + my typo in keys

--- Backend/Config/keys.js
+++ Backend/Config/keys.js
@@ -1,7 +1,7 @@
 const config = {
   secret: 'cmpe273_secret_key',
-  frontendURL: process.env.REACT_APP_FRONTEND,
+  frontendURI: process.env.REACT_APP_FRONTEND,
   mongoDB: 'mongodb+srv://admin:admin@yelp2.t9z29.mongodb.net/yelp-2?retryWrites=true&w=majority',
 };
 
-module.exports config;
+module.exports = config;
