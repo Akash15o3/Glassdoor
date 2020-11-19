6c287c17bcdd3f05f73b240ea34a9c8424e3f7b5
sunnyD777 <skohli200@gmail.com>
2020-11-18 20:41:34 -0800

    added Kafka reviews service for AWS testing

--- Backend/Routes/ReviewRoutes.js
+++ Backend/Routes/ReviewRoutes.js
@@ -1,69 +1,59 @@
 const express = require('express');
-const mongoose = require('mongoose');
-const redis = require('redis');
-const { mongoDB } = require('../Config/keys');
-const Reviews = require('../Models/ReviewModel');
+// const mongoose = require('mongoose');
+// const redis = require('redis');
+// const { mongoDB } = require('../Config/keys');
+// const Reviews = require('../Models/ReviewModel');
+const kafka = require('../kafka/client');
+
 require('dotenv').config();
 
 const Router = express.Router();
 
-const options = {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  poolSize: 500,
-  bufferMaxEntries: 0,
-};
+// const options = {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+//   poolSize: 500,
+//   bufferMaxEntries: 0,
+// };
 
 // const client = redis.createClient('6379', 'ec2-52-88-160-68.us-west-2.compute.amazonaws.com');
-const client = redis.createClient(process.env.REDIS_PORT, process.env.REDIS_CLIENT);
+// const client = redis.createClient(process.env.REDIS_PORT, process.env.REDIS_CLIENT);
 
-client.on('connect', () => {
-  console.error('Connected to Redis');
-});
+// client.on('connect', () => {
+//   console.error('Connected to Redis');
+// });
 
-client.on('error', (error) => {
-  console.error(error);
-});
+// client.on('error', (error) => {
+//   console.error(error);
+// });
 
 // eslint-disable-next-line no-unused-vars
-mongoose.connect(mongoDB, options, (err, res) => {
-  if (err) {
-    console.log(err);
-    console.log('MongoDB Connection Failed');
-  } else {
-    console.log('Reviews-MongoDB Connected');
-  }
-});
+// mongoose.connect(mongoDB, options, (err, res) => {
+//   if (err) {
+//     console.log(err);
+//     console.log('MongoDB Connection Failed');
+//   } else {
+//     console.log('Reviews-MongoDB Connected');
+//   }
+// });
 
 // Add a review
 Router.post('/', (request, response) => {
   console.log('\nEndpoint POST: Add a review');
   console.log('Req Body: ', request.body);
-  const newReview = new Reviews({
-    cname: request.body.cname,
-    rheadline: request.body.rheadline,
-    rpros: request.body.rpros,
-    rcons: request.body.rcons,
-    radvice: request.body.radvice,
-    rrecommended: request.body.rrecommended,
-    routlook: request.body.routlook,
-    rceoapprove: request.body.rceoapprove,
-    rhelpful: request.body.rhelpful,
-  });
-  newReview.save((error, review) => {
-    if (error) {
+  kafka.make_request('reviewsTopic', 'ADDREVIEW', request.body, (err, result) => {
+    if (err) {
+      console.log('Reviews addreview Kafka error');
       response.writeHead(401, {
         'Content-Type': 'text/plain',
       });
-      console.log(error);
-      console.log('Error saving review');
-      response.end('Error saving review');
+      response.end('Reviews addreview Kafka error');
     } else {
-      response.writeHead(200, {
-        'Content-Type': 'application/json',
+      response.writeHead(result.status, {
+        'Content-Type': result.header,
       });
-      console.log('Sending 200');
-      response.end(JSON.stringify(review));
+      console.log(result.content);
+      response.end(result.content);
     }
   });
 });
@@ -72,55 +62,19 @@ Router.post('/', (request, response) => {
 Router.post('/cname', (request, response) => {
   console.log('\nEndpoint POST: get all reviews for a company');
   console.log('Req Body: ', request.body);
-  if (request.body.skip === undefined) {
-    request.body.skip = 0;
-  }
-  // if (request.body.limit === undefined) {
-  //   request.body.limit = 10;
-  // }
-  request.body.limit = 10;
-  console.log('Req Body: ', request.body);
-  const redisKey = `${request.body.cname}_Reviews_${request.body.skip}`;
-
-  client.get(redisKey, (err, reply) => {
+  kafka.make_request('reviewsTopic', 'GETREVIEWBYCNAME', request.body, (err, result) => {
     if (err) {
-      console.log(err);
-    }
-    if (reply !== null) {
-      // Response exists inn the cache
-      response.writeHead(200, {
-        'Content-Type': 'application/json',
+      console.log('Reviews getreviewbycname Kafka error');
+      response.writeHead(401, {
+        'Content-Type': 'text/plain',
       });
-      console.log('Sending 200 from Redis');
-      response.end(JSON.parse(JSON.stringify(reply)));
+      response.end('Reviews getreviewbycname Kafka error');
     } else {
-      // Response not in cache--fetch from mongo
-      Reviews.find({ cname: request.body.cname })
-        .skip(0)
-        .limit(100)
-        .exec((error, reviews) => {
-          if (error) {
-            response.writeHead(401, {
-              'Content-Type': 'text/plain',
-            });
-            console.log('Error fetching reviews');
-            response.end('Error fetching reviews');
-          } else {
-            const redisValue = JSON.stringify(reviews);
-            client.set(redisKey, redisValue, (e, r) => {
-              if (e) {
-                console.log(e);
-              } else {
-                console.log('Cache successful: ', r);
-              }
-            });
-            response.writeHead(200, {
-              'Content-Type': 'application/json',
-            });
-            console.log('Sending 200 from Mongo');
-            response.end(JSON.stringify(reviews));
-          }
-        });
+      response.writeHead(result.status, {
+        'Content-Type': result.header,
+      });
+      console.log(result.content);
+      response.end(result.content);
     }
   });
 });
---
+++ kafka-backend/Services/reviews.js
@@ -0,0 +1,129 @@
+const redis = require('redis');
+const Reviews = require('../Models/ReviewModel');
+
+const client = redis.createClient(process.env.REDIS_PORT, process.env.REDIS_CLIENT);
+
+client.on('connect', () => {
+  console.error('Connected to Redis');
+});
+
+client.on('error', (error) => {
+  console.error(error);
+});
+
+function addReview(data, callback) {
+  const newReview = new Reviews({
+    cname: data.cname,
+    rheadline: data.rheadline,
+    rpros: data.rpros,
+    rcons: data.rcons,
+    radvice: data.radvice,
+    rrecommended: data.rrecommended,
+    routlook: data.routlook,
+    rceoapprove: data.rceoapprove,
+    rhelpful: data.rhelpful,
+  });
+  newReview.save((error, review) => {
+    if (error) {
+      const response = {
+        status: 401,
+        header: 'text/plain',
+        content: 'Error saving review',
+      };
+      callback(null, response);
+    } else {
+      const response = {
+        status: 200,
+        header: 'application/json',
+        content: JSON.stringify(review),
+      };
+      callback(null, response);
+    }
+  });
+}
+
+function getByCompnayName(data, callback) {
+  if (data.skip === undefined) {
+    data.skip = 0;
+  }
+  // if (data.limit === undefined) {
+  //   data.limit = 10;
+  // }
+  data.limit = 10;
+  console.log('Req Body: ', data);
+  const redisKey = `${data.cname}_Reviews_${data.skip}`;
+
+  client.get(redisKey, (err, reply) => {
+    if (err) {
+      console.log(err);
+    }
+    if (reply !== null) {
+      // Response exists inn the cache
+      const response = {
+        status: 200,
+        header: 'application/json',
+        content: JSON.parse(JSON.stringify(reply)),
+      };
+      console.log('Sending 200 from Redis');
+      callback(null, response);
+    } else {
+      // Response not in cache--fetch from mongo
+      Reviews.find({ cname: data.cname })
+        .skip(0)
+        .limit(100)
+        .exec((error, reviews) => {
+          if (error) {
+            const response = {
+              status: 401,
+              header: 'text/plain',
+              content: 'Error fetching reviews',
+            };
+            callback(null, response);
+          } else {
+            const redisValue = JSON.stringify(reviews);
+            client.set(redisKey, redisValue, (e, r) => {
+              if (e) {
+                console.log(e);
+              } else {
+                console.log('Cache successful: ', r);
+              }
+            });
+            console.log('Sending 200 from Mongo');
+            const response = {
+              status: 200,
+              header: 'application/json',
+              content: JSON.stringify(reviews),
+            };
+            callback(null, response);
+          }
+        });
+    }
+  });
+}
+
+function handleRequest(msg, callback) {
+  switch (msg.subTopic) {
+    case 'ADDREVIEW': {
+      console.log('KB: Inside add review');
+      console.log('Message:', msg);
+      addReview(msg.data, callback);
+      break;
+    }
+    case 'GETREVIEWBYCNAME': {
+      console.log('KB: Inside get review by company name');
+      console.log('Message:', msg);
+      getByCompnayName(msg.data, callback);
+      break;
+    }
+    default: {
+      const response = {
+        status: 400,
+        header: 'text/plain',
+        content: 'Bad request',
+      };
+      callback(null, response);
+    }
+  }
+}
+
+exports.handleRequest = handleRequest;
