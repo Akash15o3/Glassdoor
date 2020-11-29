30b8d956b028df3552544650fdc37e71620788e0
Akash15o3 <aggarwalakash15@gmail.com>
2020-11-29 10:16:57 -0800

    added student profile route

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,20 +1,21 @@
-import React from 'react';
-import { Route, BrowserRouter } from 'react-router-dom';
-import './Static/Styles/App.css';
-import Navbar from './Components/Navbar';
-import Login from './Components/Credentials/Login';
-import Signup from './Components/Credentials/Signup';
-import Home from './Components/Student/Home/Home';
-import Employer from './Components/Employer/Employer';
-import EmpProfile from './Components/Employer/Home/Home';
+import React from "react";
+import { Route, BrowserRouter } from "react-router-dom";
+import "./Static/Styles/App.css";
+import Navbar from "./Components/Navbar";
+import Login from "./Components/Credentials/Login";
+import Signup from "./Components/Credentials/Signup";
+import Home from "./Components/Student/Home/Home";
+import Employer from "./Components/Employer/Employer";
+import EmpProfile from "./Components/Employer/Home/Home";
 // import EmpProfile from "./Components/Employer/Profile";
-import Admin from './Components/Admin/Admin';
-import AdminCompany from './Components/Admin/AdminCompanyPage';
-import Jobs from './Components/Employer/jobs';
-import CompanyHomePage from './Components/Student/Company/CompanyHomePage';
-import CompanySearchResults from './Components/Student/Company/CompanySearchResults';
-import JobSearchResults from './Components/Student/Jobs/JobSearchResults';
-import JobApplications from './Components/Student/Jobs/JobApplications';
+import Admin from "./Components/Admin/Admin";
+import AdminCompany from "./Components/Admin/AdminCompanyPage";
+import Jobs from "./Components/Employer/jobs";
+import CompanyHomePage from "./Components/Student/Company/CompanyHomePage";
+import CompanySearchResults from "./Components/Student/Company/CompanySearchResults";
+import JobSearchResults from "./Components/Student/Jobs/JobSearchResults";
+import JobApplications from "./Components/Student/Jobs/JobApplications";
+import StudentProfile from "./Components/Employer/studentProfile";
 
 function App() {
   return (
@@ -28,8 +29,12 @@ function App() {
         <Route exact path="/admin" component={Admin} />
         <Route exact path="/admin/company" component={AdminCompany} />
         <Route path="/employerJobs" component={Jobs} />
+        <Route path="/studentProfile/:aapplierid" component={StudentProfile} />
         <Route path="/student/company" component={CompanyHomePage} />
-        <Route path="/student/companySearchResults" component={CompanySearchResults} />
+        <Route
+          path="/student/companySearchResults"
+          component={CompanySearchResults}
+        />
         <Route path="/student/jobSearchResults" component={JobSearchResults} />
         <Route path="/student/jobApplications" component={JobApplications} />
       </div>
