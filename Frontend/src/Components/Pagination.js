83664c3afa01153b0cd701e172214ca615d9f4bd
sunnyD777 <skohli200@gmail.com>
2020-12-01 14:13:16 -0800

    modified Pagination

--- Frontend/src/Components/Pagination.js
+++ Frontend/src/Components/Pagination.js
@@ -1,45 +1,27 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-export default class Pagination extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-
-    };
-  }
-
-  render() {
-    const { page, numPages, setPage } = this.props;
-    return (
-      <div id="FooterPageNav" className="pageNavBar tbl fill noMargBot" style={{ display: 'flex', justifyContent: 'center' }}>
-        <div className="pagingControls cell middle">
-          <ul>
-            <li onClick={setPage} className="prev">
-              {page === 0
-                ? <span className="disabled"><i><span>Previous</span></i></span>
-                : <a><i><span>Previous</span></i></a>}
-            </li>
-            {[...Array(numPages)].map((e, i) => {
-              return i === page ? <li onClick={setPage} pageIndex={i} className="page current "><span className="disabled">{i + 1}</span></li>
-                : <li onClick={setPage} pageIndex={i} className={`page ${numPages - 1 === i ? 'last' : ''}`}><a>{i + 1}</a></li>;
-            })}
-            {/* {[...Array(numPages)].map((e, i) => {
-              return (
-                <li onClick={setPage} pageIndex={i} className={`page ${page === i ? 'current' : ''} ${numPages - 1 === i ? 'last' : ''}`}>
-                  { i === page
-                    ? <span className="disabled">{i + 1}</span>
-                    : <a>{i + 1}</a>}
-                </li>
-              );
-            })} */}
-            <li onClick={setPage} className="next">
-              {page === numPages - 1
-                ? <span className="disabled"><i><span>Next</span></i></span>
-                : <a><i><span>Next</span></i></a>}
-            </li>
-          </ul>
-        </div>
+export default function Pagination(props) {
+  const { page, numPages, setPage } = props;
+  return (
+    <div id="FooterPageNav" className="pageNavBar tbl fill noMargBot" style={{ display: 'flex', justifyContent: 'center' }}>
+      <div className="pagingControls cell middle">
+        <ul>
+          <li onClick={setPage} className="prev">
+            {page === 0
+              ? <span className="disabled"><i><span>Previous</span></i></span>
+              : <a><i><span>Previous</span></i></a>}
+          </li>
+          {[...Array(numPages)].map((e, i) => {
+            return i === page ? <li onClick={setPage} pageIndex={i} className="page current "><span className="disabled">{i + 1}</span></li>
+              : <li onClick={setPage} pageIndex={i} className={`page ${numPages - 1 === i ? 'last' : ''}`}><a>{i + 1}</a></li>;
+          })}
+          <li onClick={setPage} className="next">
+            {page === numPages - 1
+              ? <span className="disabled"><i><span>Next</span></i></span>
+              : <a><i><span>Next</span></i></a>}
+          </li>
+        </ul>
       </div>
-    );
-  }
+    </div>
+  );
 }
